e7639d5baab6611d6cf55dc94bc433ca99a959d8
harishkumarforeverr <harishking809@gmail.com>
2023-03-24 16:05:24 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-// run `node index.js` in the terminal
-console.log = (arg) => arg;
-global.console.log(`Hello Node.js v${process.versions.node}!`);
+var XLSX = require('xlsx');
+var workbook = XLSX.readFile('Master.xlsx');
+var sheet_name_list = workbook.SheetNames;
+var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+console.log(xlData);
