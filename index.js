2b5b6564c01ae565afda888d6969e6bb462b56a9
harishkumarforeverr <harishking809@gmail.com>
2023-03-24 16:06:50 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var XLSX = require('xlsx');
-var workbook = XLSX.readFile('Master.xlsx');
+var workbook = XLSX.readFile('test.xlsx');
 var sheet_name_list = workbook.SheetNames;
 var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-console.log(xlData);
+console.log(workbook);
